e0a7163d6649c13610601707dc2628694d486a6d
Nicolas Kagami <nicolaskagami@gmail.com>
2022-08-06 22:13:11 -0300

    New auth strategy for angularx google oauth2

--- reyceitas-backend/index.js
+++ reyceitas-backend/index.js
@@ -2,77 +2,52 @@ require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const recipes = require('./routes/recipes');
 const app = express();
 const cors = require('cors');
-const passport = require("passport");
+const {OAuth2Client} = require('google-auth-library');
 
-
-require("./config/google");
-require("./config/passport");
-require("./config/google");
+const client = new OAuth2Client(process.env.CLIENT_ID);
+const jwt = require('jsonwebtoken');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.engine("html", require("ejs").renderFile);
-app.use(express.static(__dirname + "/public"));
-
-app.use(cookieParser());
-
-const flash = require("express-flash");
-const session = require("express-session");
-
-app.use(
-  session({
-    secret: "secr3t",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use(flash());
 app.use('/api/recipes', recipes);
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+
+const isLoggedIn = (req, res, next) => {
+  req.user ? next() : res.sendStatus(401);
+};
+  
+app.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile.ejs", { user: req.user });
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cors());
-app.get(
-  "/auth/google", cors(),
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-
-app.get(
-  "/auth/google/callback", cors(),
-  passport.authenticate("google", {
-    failureRedirect: "/",
-    successRedirect: "/profile",
-    failureFlash: true,
-    successFlash: "Successfully logged in!",
-  })
-);
-const isLoggedIn = (req, res, next) => {
-    req.user ? next() : res.sendStatus(401);
-  };
-  
-  app.get("/profile", isLoggedIn, (req, res) => {
-    res.render("profile.ejs", { user: req.user });
+app.get("/auth/logout", (req, res) => {
+  req.flash("success", "Successfully logged out");
+  req.session.destroy(function () {
+    res.clearCookie("connect.sid");
+    res.redirect("/");
   });
-
-  app.get("/auth/logout", (req, res) => {
-    req.flash("success", "Successfully logged out");
-    req.session.destroy(function () {
-      res.clearCookie("connect.sid");
-      res.redirect("/");
-    });
-  });
+});
+app.get("/login", (req,res,next) => {
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken : req.body.token,
+            audience : process.env.CLIENT_ID
+        });
+        const payload = ticket.getPayload();
+        const userDetails = {
+            email : payload['email'],
+            firstname : payload['given_name'],
+            lastname : payload['family_name']
+        }
+        let token = jwt.sign(userDetails, process.env.CLIENT_SECRET, {expiresIn: 1440});
+        res.status(200).json({ token: token })
+    }
+    verify().catch(console.error);
+})
 db = 'mongodb://db:27017/reyceitas-mean'
 mongoose.connect(
     db,
