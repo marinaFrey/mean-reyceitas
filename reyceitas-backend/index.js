9dceaa00ecf59221d90e2db2dbe0e6832c0b1827
Nicolas Kagami <nicolaskagami@gmail.com>
2022-08-07 22:08:47 -0300

    Verify JWT on recipes and picture on JWT

--- reyceitas-backend/index.js
+++ reyceitas-backend/index.js
@@ -3,22 +3,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const recipes = require('./routes/recipes');
-const app = express();
 const cors = require('cors');
 const {OAuth2Client} = require('google-auth-library');
+const jwt = require('jsonwebtoken');
+const app = express();
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
-const jwt = require('jsonwebtoken');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api/recipes', recipes);
-
 const isLoggedIn = (req, res, next) => {
   req.user ? next() : res.sendStatus(401);
 };
+
+function verifyJWT(req, res, next){
+    const token = req.headers['token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, process.env.CLIENT_SECRET, function(err, decoded) {
+      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      
+      req.userId = decoded.id;
+      console.log(decoded)
+      next();
+    });
+}
+app.use('/api/recipes', verifyJWT, recipes);
   
 app.get("/profile", isLoggedIn, (req, res) => {
   res.render("profile.ejs", { user: req.user });
@@ -31,6 +43,21 @@ app.get("/auth/logout", (req, res) => {
     res.redirect("/");
   });
 });
+app.get('/auth', (req, res) => {
+    const token = req.body.token;
+    if(token) {
+        const decode = jwt.verify(token, process.env.CLIENT_SECRET);
+        res.json({
+            login: true,
+            data: decode
+        });
+    } else {
+        res.json({
+            login: false,
+            data: 'error'
+        });
+    }
+});
 app.post("/login", (req,res,next) => {
     async function verify() {
         const ticket = await client.verifyIdToken({
@@ -40,13 +67,14 @@ app.post("/login", (req,res,next) => {
         const payload = ticket.getPayload();
         const userDetails = {
             email : payload['email'],
-            firstname : payload['given_name'],
-            lastname : payload['family_name']
+            profilePicture : payload['picture'],
+            firstName : payload['given_name'],
+            lastName : payload['family_name']
         }
         let token = jwt.sign(userDetails, process.env.CLIENT_SECRET, {expiresIn: 1440});
         res.status(200).json({ token: token })
     }
-    verify().catch(res.status(401));
+    verify().catch((e) => {res.status(401).json({"Error":"Not authorized"})});
 })
 db = 'mongodb://db:27017/reyceitas-mean'
 mongoose.connect(
