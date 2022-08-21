dddb93cef8b956d61499b1006398185f8069887f
Nicolas Kagami <nicolaskagami@gmail.com>
2022-08-21 18:56:17 -0300

    Tag Color

--- reyceitas-backend/models/Tag.js
+++ reyceitas-backend/models/Tag.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const tagSchema = new Schema({
   name:         { type: String, required: true, index: true, unique:true },
+  color:         { type: String }
 })
 
 module.exports = mongoose.model('tag', tagSchema);
