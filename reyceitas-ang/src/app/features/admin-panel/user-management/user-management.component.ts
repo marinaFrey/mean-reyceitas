e74341fd39da5669559af332382fc0874efb1cfb
marinaFrey <marina.fortes.rey@gmail.com>
2022-08-25 21:48:36 -0300

    alters angular local env to use https

--- reyceitas-ang/src/app/features/admin-panel/user-management/user-management.component.ts
+++ reyceitas-ang/src/app/features/admin-panel/user-management/user-management.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@models/user.model';
+import { UserManagementService } from '@services/user-management.service';
+import { map, take } from 'rxjs';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.scss']
+  styleUrls: ['./user-management.component.scss'],
+  providers: [UserManagementService]
 })
 export class UserManagementComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(private userService: UserManagementService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().pipe(
+      take(1),
+      map((users => this.users = users))
+    ).subscribe()
   }
 
 }
