d73bd7b8fb771a3733a4bebac8c9a4f9e7347d23
Nicolas Kagami <nicolaskagami@gmail.com>
2023-02-16 11:14:04 -0300

    responds 401 on invalid/expired JWT

--- reyceitas-backend/config/auth.js
+++ reyceitas-backend/config/auth.js
@@ -6,7 +6,7 @@ function verifyJWT(req, res, next){
     const token = authHeader[1];
     
     jwt.verify(token, process.env.CLIENT_SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
       req.userId = decoded.id;
       next();
     });
